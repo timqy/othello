ffd7cac0a38157e8b4a8bf81099b758014fe4741
timqy <dv13thg@cs.umu.se>
2015-11-16 14:24:51 +0100

    so

--- src/Othello.java
+++ src/Othello.java
@@ -3,11 +3,30 @@
  */
 public class Othello {
 
+    public static float time;
+
     public static void main(String[] args) {
-        OthelloAlgorithm algorithm = new AlphaBeta();
-        OthelloEvaluator evaluator = new HeuristicEvaluator();
-        algorithm.setEvaluator(evaluator);
-        algorithm.setSearchDepth(5);
+        if(args.length != 3){
+            System.err.println("Othello runs on two arguments");
+            System.exit(-1);
+        }
+
+
+        if(args[1].length() != 65 &&
+                (args[1].charAt(0) == 'W' || args[1].charAt(0) == 'B')){
+            System.err.println("First input should contain 65 characters and start with W or B");
+            System.exit(-1);
+        }
+
+        OthelloAlgorithm alphaBetaSearch = new AlphaBeta();
+        OthelloEvaluator heuristicEvaluator = new HeuristicEvaluator();
+        alphaBetaSearch.setEvaluator(heuristicEvaluator);
+
+        time = Float.parseFloat(args[2]);
+
+        OthelloPosition othelloBoard = new OthelloPosition(args[1]);
+
+        alphaBetaSearch.evaluate(othelloBoard);
 
     }
 }
