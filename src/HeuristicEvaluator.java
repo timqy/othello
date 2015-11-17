245b7baa7b3f1d63b8654d0cc9781d1282657ccd
Tim Hedberg <dv13thg@cs.umu.se>
2015-11-17 11:53:08 +0100

    Update HeuristicEvaluator.java
    added some functions :)

--- src/HeuristicEvaluator.java
+++ src/HeuristicEvaluator.java
@@ -20,4 +20,58 @@ public class HeuristicEvaluator implements OthelloEvaluator {
 
         return maxValue;
     }
+    
+    public int coinParity(OthelloPosition position){
+        int blackCoin;
+        int whiteCoin;
+        
+        for(int x = 1; position.IsWithinBoard(x);x++){
+            for(int y = 1; position.IsWithinBoard(y);y++){
+                if(position.board[x][y] == 'X')
+                    blackCoin++;
+                if(position.board[x][y] == 'O')
+                    whiteCoin++;
+            }
+        }
+        
+        
+        return 100* (whiteCoin /blackCoin);
+    }
+    
+    /**
+     * Corner positions are nice.
+     */
+    public boolean isInCorner(OthelloAction action){
+        int actionX = action.getRow();
+        int actionY = action.getColumn();
+        
+        int cornerX = 0+1;
+        int cornerY = OthelloPosition.BOARD_SIZE;
+        
+        if(actionX == cornerX || actionX == cornerY)
+            if(actionY == cornerX || actionY == cornerY)
+                return true;
+        
+        return false;
+    }
+    
+    /**
+     *  If an action is oriented around a corner it gives the oponent
+     * the opportunity to place a marker in corner.
+     * 
+     */
+    public boolean isAroundCorner(OthelloAction action,OthelloPosition position){
+        int actionX = action.getRow();
+        int actionY = action.getColumn();
+        
+        int lowerSide = 0+1;
+        int upperSide = OthelloPosition.BOARD_SIZE;
+        
+        if(actionX == lowerside || actionX == lowerSide+1 ||actionX == upperSide ||actionX == upperSide+1)
+            if(actionY == lowerside || actionY == lowerSide+1 ||actionY == upperSide ||actionY == upperSide+1)
+                if((actionX != lowerSide  && actionX != upperSide) && (actionY != lowerSide || actionY != upperSide))
+                    return true;
+
+        return false;
+    }
 }
